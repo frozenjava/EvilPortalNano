5f122afe57977664086bf0ffe14268f7789d969a
H4xl0r <31049432+H4xl0r@users.noreply.github.com>
2019-03-18 14:49:55 +0100

    Delete settings.php

--- EvilPortal/includes/mail/settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-$cfg_server   = 'server here'; //smtp.gmail.com
-$cfg_port     =  'port'; // 587 (for TLS) 465 (for SSL) 25 (No secure)
-$cfg_secure   = 'ssl'; //tls , ssl or "" 
-$cfg_username = 'username here'; 
-$cfg_password = 'password here';
-?>

9018df239ada9f943f8d9799b598322d39a8efad
H4xl0r <31049432+H4xl0r@users.noreply.github.com>
2019-03-18 11:30:11 +0100

    Create settings.php

---
+++ EvilPortal/includes/mail/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+$cfg_server   = '{$_POST['server']}';
+$cfg_port     =  {$_POST['port']};
+$cfg_secure   = '{$_POST['secure']}';
+$cfg_username = '{$_POST['usernm']}';
+$cfg_password = '{$_POST['passwd']}';
+
+?>

